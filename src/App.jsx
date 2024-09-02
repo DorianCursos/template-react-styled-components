5fbe8e30284353e1e31b68b37fd6ab4880fcfbda
DorianCursos <doriancursos@gmail.com>
2024-09-02 06:55:17 +0200

    template update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Button from './components/button/Button';
+import { GlobalStyles } from './styles/GlobalStyles';
+
 const App = () => {
 	return (
 		<div>
+			<GlobalStyles />
 			<h1>Curso de React</h1>
+			<Button color='steelblue'>Click Me!</Button>
+			<Button color='#f0f'>Click Me!</Button>
 		</div>
 	);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles/normalize.css';
-import './styles/index.css';
 
 const container = document.getElementById('root');
 
